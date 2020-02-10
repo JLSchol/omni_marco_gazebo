7b7da6d3459323de9f052b9801454d20ec10b5e0
Jasper Schol <jasper.schol@live.nl>
2020-02-10 18:54:17 +0100

    plot and subplot without layout

--- src/data_processing/scripts/PlotData.py
+++ src/data_processing/scripts/PlotData.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gs
 import numpy as np
 from ImportFiles import ImportFiles
 from ProcessFiles import ProcessFiles
 from PlotTopicInfo import PlotTopicInfo
+
+# /home/jasper/omni_marco_gazebo/src/data_processing/test/202001301532_D5_W200_L0.03_0.2_S100_1000
+# dfList = [csvsPandas[3],csvsPandas[6],csvsPandas[9]]
+# nameList = [csvAndTfNames[3],csvAndTfNames[6],csvAndTfNames[9]]
+
+# /home/jasper/omni_marco_gazebo/src/data_processing/test/202002061052_D5_W200_L0.03_0.2_S100_1000
+# dfList = [csvsPandas[3],csvsPandas[6],csvsPandas[9]]
+# nameList = [csvAndTfNames[3],csvAndTfNames[6],csvAndTfNames[9]]
+
+# /home/jasper/omni_marco_gazebo/src/data_processing/test/202002061043_D30_W200_L0.03_0.2_S100_1000
+# dfList = [csvsPandas[3],csvsPandas[6],csvsPandas[10],csvsPandas[13],csvsPandas[14]]
+# nameList = [csvAndTfNames[3],csvAndTfNames[6],csvAndTfNames[10],csvAndTfNames[13],csvAndTfNames[14]]
+
+
+# "/home/jasper/omni_marco_gazebo/src/data_processing/data/202001140849_D30_W100_L0.01_0.45_S100_1000" not working
+# set correct directories
 
 class PlotData(object):
 	def __init__(self):
@@ -26,60 +43,65 @@ class PlotData(object):
 
 
 
-	def simpleFigure(self,df,figureInfo):
+	def simpleFigure(self,df,figureInfo,fig=False,ax=False):
 
-		# if plt == 'empty':
-		# 	plt.plot()
-			# print(type(plt))
+		if fig==False and ax==False:
+			fig,ax = plt.subplots()
 
+		# get the info
 		title = figureInfo.get('title')
-		# print(type(title))
 		legend = figureInfo.get('legend')
-
 		X = figureInfo.get('xAxis')
 		Ys = figureInfo.get('yAxis')
-		# print(figureInfo)
 		xlabel = figureInfo.get('xLabel')
 		ylabel = figureInfo.get('yLabel')
-		# print(dfYColumns)
-		# print(ylabel)
-
-		plt.plot()
-		plt.title(title)
-		print(Ys)
+	
+		# set in figure
+		ax.set_title(title)
 		for i, Y in enumerate(Ys):
-			# map
-			# print(df[X])
 			x = map(lambda x: x,  df[X])
 			y = map(lambda x: x,  df[Y])
-			plt.plot(x,y,label=legend[i])
-		plt.xlabel(xlabel)
-		plt.ylabel(ylabel)
-		plt.legend(loc='best')
-		plt.grid()
+			ax.plot(x,y,label=legend[i])
+		ax.set_xlabel(xlabel)
+		ax.set_ylabel(ylabel)
+		ax.legend(loc='best')
+		ax.grid()
 
-		return plt
+		return fig,ax
 
+	def addSubPlot(self,df,figureInfo,fig,ax,position='row'):
+		geometry=ax.get_geometry()
+		geometry1 = []
+		geometry2 = []
+		nrows = []
+		ncols = []
 
+		tupleAdd = lambda t1,t2: tuple(np.array(t1)+np.array(t2))
+		if position == 'row':
+			geometry1 = tupleAdd((1,0,0),geometry)
+			nrows =geometry1[0]
+			ncols =geometry1[1]
+		elif position == 'col':
+			geometry1 = tupleAdd((0,1,0),geometry)
+			nrows =geometry1[0]
+			ncols =geometry1[1]
+		else:
+			print("{} is not a valid input. \n Use 'row' or 'col'".format(position))
+
+		geometry2 = tupleAdd((0,0,1),geometry1)
+
+		ax.change_geometry(*geometry1)
+
+		ax = fig.add_subplot(*geometry2)
+		# set on gridspec
+		fig,ax = self.simpleFigure(df,figureInfo,fig,ax) 
+
+		return fig,ax
 
 
 
 if __name__== "__main__":
-	# /home/jasper/omni_marco_gazebo/src/data_processing/test/202001301532_D5_W200_L0.03_0.2_S100_1000
-	# dfList = [csvsPandas[3],csvsPandas[6],csvsPandas[9]]
-	# nameList = [csvAndTfNames[3],csvAndTfNames[6],csvAndTfNames[9]]
 
-	# /home/jasper/omni_marco_gazebo/src/data_processing/test/202002061052_D5_W200_L0.03_0.2_S100_1000
-	# dfList = [csvsPandas[3],csvsPandas[6],csvsPandas[9]]
-	# nameList = [csvAndTfNames[3],csvAndTfNames[6],csvAndTfNames[9]]
-
-	# /home/jasper/omni_marco_gazebo/src/data_processing/test/202002061043_D30_W200_L0.03_0.2_S100_1000
-	# dfList = [csvsPandas[3],csvsPandas[6],csvsPandas[10],csvsPandas[13],csvsPandas[14]]
-	# nameList = [csvAndTfNames[3],csvAndTfNames[6],csvAndTfNames[10],csvAndTfNames[13],csvAndTfNames[14]]
-
-
-	# "/home/jasper/omni_marco_gazebo/src/data_processing/data/202001140849_D30_W100_L0.01_0.45_S100_1000" not working
-	# set correct directories
 	IF = ImportFiles("/home/jasper/omni_marco_gazebo/src/data_processing/test","202002061043_D30_W200_L0.03_0.2_S100_1000") #,"202001131800_D30_W100_L0.01_0.45_S100_1000"
 	csvsPandas,csvNames,yamlDict,yamlNames = IF.importAll()
 
@@ -88,35 +110,34 @@ if __name__== "__main__":
 	# process dict and csvs
 	paramDict = PF.trimParamDict(yamlDict[1],['rosdistro','roslaunch','run_id','rosversion'])
 	csvsPandas,csvAndTfNames = PF.processCsvs(csvsPandas,csvNames)
-	print(csvAndTfNames)
+	# print(csvAndTfNames)
 
 	# set class variables not necessary
 	PF.setParams(csvsPandas,csvAndTfNames,paramDict)
-
 
 	# get list of things I want to plot
 	# print(len(csvAndTfNames))
 	dfList = [csvsPandas[3],csvsPandas[6],csvsPandas[10],csvsPandas[13],csvsPandas[14]]
 	nameList = [csvAndTfNames[3],csvAndTfNames[6],csvAndTfNames[10],csvAndTfNames[13],csvAndTfNames[14]]
-	# print(nameList)
+	print(nameList)
 
 	
 	# init classes
 	TI = PlotTopicInfo()
 	PD = PlotData()
 
-	for i,name in enumerate(nameList):
-		info = TI.getInfo(name)
-		figure = PD.simpleFigure(dfList[i],info)
-		figure.show()
+	info = TI.getInfo(nameList[0])
+	fig,ax = PD.simpleFigure(dfList[0],info)
+	fig,ax = PD.addSubPlot(dfList[1],TI.getInfo(nameList[1]),fig,ax)
+	fig,ax = PD.addSubPlot(dfList[2],TI.getInfo(nameList[2]),fig,ax)
+	# left  = 0.125  # the left side of the subplots of the figure
+	# right = 0.9    # the right side of the subplots of the figure
+	# bottom = 0.1   # the bottom of the subplots of the figure
+	# top = 0.9      # the top of the subplots of the figure
+	# wspace = 0.2   # the amount of width reserved for blank space between subplots
+	# hspace = 0.5   # the amount of height reserved for white space between subplots
+	# plt.subplots_adjust(left=None, bottom=bottom, right=None, top=top, wspace=None, hspace=hspace)
 
-
-
-
-	# dfXLable = 'timeVec'
-	# dfYLables = ['field.F32MA.data0','field.F32MA.data4','field.F32MA.data8']
-
-
-	# Kfig = PD.basicPlot(stiffness_command,dfXLable,dfYLables)
-	# Kfig.title("Human commanded Stiffness diagonals")
-	# Kfig.show()
+	# X = 10*np.random.rand(5,3)
+	
+	plt.show()
