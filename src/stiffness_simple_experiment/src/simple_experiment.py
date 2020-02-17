8c16c22cf3e0121b90c94ce560028c7d22ccd166
Jasper Schol <jasper.schol@live.nl>
2020-02-17 14:47:04 +0100

    made importable python file, setup simple_experiment .py

--- src/stiffness_simple_experiment/src/simple_experiment.py
+++ src/stiffness_simple_experiment/src/simple_experiment.py
@@ -3,17 +3,32 @@
 from rospy import init_node, Publisher, Subscriber, is_shutdown, Rate, loginfo, spin
 from rospy import ROSInterruptException, Time, get_param, has_param, logwarn, logfatal
 # custom class
-from ellipsoid_message import EllipsoidMessage
+from stiffness_visualization.ellipsoid_message import EllipsoidMessage
+# 
+from std_msgs.msg import String
 
 class SimpleExperiment(object):
     def __init__(self):
-        pass
+        init_node("simple_experiment",anonymous=True)
+        self._getParameters()
+
+        self._guiSub = Subscriber("gui_command", String, self._guiCallBack)
+        self._logPub = Publisher("simple_experiment_logger", String,queue_size=1)
+
+        
 
     def run(self):
         while not is_shutdown:
             print("hoi")
 
 
+
+
+    def _getParameters(self):
+        pass
+
+    def _guiCallBack(self, message):
+        self.guiMsg = message
 
 
 
