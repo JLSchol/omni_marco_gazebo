4a89b2171da799b9268dd300921d5a39a2825b78
Jasper Schol <jasper.schol@live.nl>
2020-08-03 14:29:29 +0200

    generate plots in analysis

--- src/stiffness_simple_experiment/Analysis.py
+++ src/stiffness_simple_experiment/Analysis.py
@@ -9,7 +9,7 @@ class AnalyseSimpleExperiment():
 	def __init__(self):
 		pass
 
-	def generateRawDataPlots(self, all_dfs, base_path):
+	def generateRawDataPlots(self, all_dfs, base_path, plot_part='all'):
 		plot_exp = PlotSimpleExperiment()
 		exp_id = {
 					1: '1_DoF_Front_Practice',
@@ -22,84 +22,90 @@ class AnalyseSimpleExperiment():
 					8: '2_DoF_Horizon_Real'
 					}
 		part_nr = 0
-		for part_dfs in all_dfs:
+		for part_dfs in all_dfs: # for loop over all participants participants
 			part_nr +=1
-			for exp_nr,df in part_dfs.items():
-				# (1) plot trial times
-				# copy and set custom legends
-				time1_info = dict(plot_exp.trial_time) 
-				time1_info['legends'] = [exp_id[exp_nr]]
-				# plot using custom legend
-				fig_time,ax_time = plot_exp.singlePlotDf(df, time1_info)
+			for plot_part_nr in plot_part:
+				if plot_part == 'all' or plot_part_nr == part_nr: 
+					for exp_nr,df in part_dfs.items(): # loop over 8 conditions
+						# (1) plot trial times
+						# copy and set custom legends
+						time1_info = dict(plot_exp.trial_time) 
+						time1_info['legends'] = [exp_id[exp_nr]]
+						# plot using custom legend
+						fig_time,ax_time = plot_exp.singlePlotDf(df, time1_info)
 
-				# add trial times plot from other experiment (1-2)
-				# set new color for line
-				# time2_info = dict(time1_info)
-				# time2_info['colors'] = ['r']
-				# time2_info['legends'] = [exp_id[exp_nr]]
-				# # add line to figure using other color
-				# ax_time = plot_exp.addLineFromDfToPlot(ax_time, df, time2_info)
+						# add trial times plot from other experiment (1-2)
+						# set new color for line
+						# time2_info = dict(time1_info)
+						# time2_info['colors'] = ['r']
+						# time2_info['legends'] = [exp_id[exp_nr]]
+						# # add line to figure using other color
+						# ax_time = plot_exp.addLineFromDfToPlot(ax_time, df, time2_info)
 
-				# (2) plot accuracy
-				acc_info = dict(plot_exp.accuracy)
-				acc_info['title'] = exp_id[exp_nr]
-				fig_acc, ax_acc = plot_exp.singlePlotDf(df, acc_info)
+						# (2) plot accuracy
+						acc_info = dict(plot_exp.accuracy)
+						acc_info['title'] = exp_id[exp_nr]
+						fig_acc, ax_acc = plot_exp.singlePlotDf(df, acc_info)
 
-				# (3) plot shape error (diameter)
-				shape_error_info = dict(plot_exp.shape_error)
-				shape_error_info['title'] = exp_id[exp_nr]
-				fig_shape, ax_shape = plot_exp.singlePlotDf(df, shape_error_info)
+						# (3) plot shape error (diameter)
+						shape_error_info = dict(plot_exp.shape_error)
+						shape_error_info['title'] = exp_id[exp_nr]
+						fig_shape, ax_shape = plot_exp.singlePlotDf(df, shape_error_info)
 
-				# (4) plot average shape error (diameter)
-				avg_shape_error_info = dict(plot_exp.avg_shape_error)
-				fig_avgShape, ax_avgShape = plot_exp.singlePlotDf(df, avg_shape_error_info)
+						# (4) plot average shape error (diameter)
+						avg_shape_error_info = dict(plot_exp.avg_shape_error)
+						fig_avgShape, ax_avgShape = plot_exp.singlePlotDf(df, avg_shape_error_info)
 
-				# (5) plot angle
-				angle_info = dict(plot_exp.abs_angle)
-				fig_angle, ax_angle = plot_exp.singlePlotDf(df, angle_info)
+						# (5) plot angle
+						angle_info = dict(plot_exp.abs_angle)
+						fig_angle, ax_angle = plot_exp.singlePlotDf(df, angle_info)
 
-				# (6) plot user scales
-				user_scales_info = dict(plot_exp.user_scales)
-				fig_us, ax_us = plot_exp.singlePlotDf(df, user_scales_info)
+						# (6) plot user scales
+						user_scales_info = dict(plot_exp.user_scales)
+						fig_us, ax_us = plot_exp.singlePlotDf(df, user_scales_info)
 
-				# (7) plot projected scales
-				proj_scales_info = dict(plot_exp.projected_scales)
-				fig_ps, ax_ps = plot_exp.singlePlotDf(df, proj_scales_info)
-				
-				# (8) plot experiment scales
-				exp_scales_info = dict(plot_exp.exp_scales)
-				fig_es, ax_es = plot_exp.singlePlotDf(df, exp_scales_info)
+						# (7) plot projected scales
+						proj_scales_info = dict(plot_exp.projected_scales)
+						fig_ps, ax_ps = plot_exp.singlePlotDf(df, proj_scales_info)
+						
+						# (8) plot experiment scales
+						exp_scales_info = dict(plot_exp.exp_scales)
+						fig_es, ax_es = plot_exp.singlePlotDf(df, exp_scales_info)
 
 
-				# (9) plot originals experiment scales
-				or_exp_scales_info = dict(plot_exp.or_user_scales)
-				fig_oes, ax_oes = plot_exp.singlePlotDf(df, or_exp_scales_info)
+						# (9) plot originals experiment scales
+						or_exp_scales_info = dict(plot_exp.or_user_scales)
+						fig_oes, ax_oes = plot_exp.singlePlotDf(df, or_exp_scales_info)
 
-				# (10) plot user quats
-				user_quats_info = dict(plot_exp.user_quats)
-				fig_uq, ax_uq = plot_exp.singlePlotDf(df, user_quats_info)
+						# (10) plot user quats
+						user_quats_info = dict(plot_exp.user_quats)
+						fig_uq, ax_uq = plot_exp.singlePlotDf(df, user_quats_info)
 
-				# (11) plot experiment quats
-				exp_quats_info = dict(plot_exp.exp_quats)
-				fig_eq, ax_eq = plot_exp.singlePlotDf(df, exp_quats_info)
+						# (11) plot experiment quats
+						exp_quats_info = dict(plot_exp.exp_quats)
+						fig_eq, ax_eq = plot_exp.singlePlotDf(df, exp_quats_info)
 
-				# (12) plot originals experiment quats
-				or_exp_quats_info = dict(plot_exp.or_user_quats)
-				fig_oeq, ax_oeq = plot_exp.singlePlotDf(df, or_exp_quats_info)
+						# (12) plot originals experiment quats
+						or_exp_quats_info = dict(plot_exp.or_user_quats)
+						fig_oeq, ax_oeq = plot_exp.singlePlotDf(df, or_exp_quats_info)
 
-				basic_figs = [fig_time,fig_acc,fig_shape,fig_avgShape,fig_angle,fig_us,fig_ps,fig_es,fig_oes,fig_uq,fig_eq,fig_oeq]
-				basic_names = ["1_trial_time","2_accuracy","3_scale_error","4_average_scales","5_angle","6_user_scales",
-				"7_proj_scales","8_exp_scales","9_exp_scales_original","10_user_quats","11_exp_quats","12_user_quats_original"]
-				
-				out_dir = base_path + str(part_nr)+"/"+exp_id[exp_nr]
+						basic_figs = [fig_time,fig_acc,fig_shape,fig_avgShape,fig_angle,fig_us,fig_ps,fig_es,fig_oes,fig_uq,fig_eq,fig_oeq]
+						basic_names = ["1_trial_time","2_accuracy","3_scale_error","4_average_scales","5_angle","6_user_scales",
+						"7_proj_scales","8_exp_scales","9_exp_scales_original","10_user_quats","11_exp_quats","12_user_quats_original"]
+						
+						out_dir = base_path + str(part_nr)+"/"+exp_id[exp_nr]
 
-				plot_exp.saveFigs(basic_figs,basic_names,out_dir,'.png')
-				plt.close('all')
-				print('RUSTAAAGH!!!, plotjes worden gemaakt')
+						plot_exp.saveFigs(basic_figs,basic_names,out_dir,'.png')
+						plt.close('all')
+						print('RUSTAAAGH!!!, plotjes worden gemaakt')
+				else:
+					continue
+
+
 		plt.close('all')
 
 
-	def generateBoxExp(self, exp_dfs, parts ,base_path, real='real'):
+	def generateBoxExp(self, exp_dfs, part_list ,base_path, real='real'):
 		plot_exp = PlotSimpleExperiment()
 
 		# some helpfull functions
@@ -113,7 +119,7 @@ class AnalyseSimpleExperiment():
 		plot_data = []
 		box_names = []
 		out_dirs = []
-		for part in parts: # over participant names 'part_1, part_2 etc'
+		for part in part_list: # over participant names 'part_1, part_2 etc'
 			plot_data_part_x = []
 
 			for cond_i in exp_dfs: # over experiment conditions 1-4
@@ -145,7 +151,7 @@ class AnalyseSimpleExperiment():
 
 		
 
-	def generateBoxTypes(self, types_dfs, id_strings, parts, base_path):
+	def generateBoxTypes(self, types_dfs, id_strings, part_list, base_path):
 		plot_exp = PlotSimpleExperiment()
 		fields = ['field.shape_acc','field.orientation_acc','field.trial_time']
 
@@ -153,7 +159,7 @@ class AnalyseSimpleExperiment():
 		get_out_dir = lambda base_path, ID, : base_path + str(ID)
 
 		box_figs = []
-		partsPlusAll = parts + ['All']
+		partsPlusAll = part_list + ['All']
 		for part in partsPlusAll:
 			box_figs_part_x =[]
 			box_names = []
@@ -186,20 +192,34 @@ class AnalyseSimpleExperiment():
 
 
 
-
 if __name__ == "__main__":
 	AE = AnalyseSimpleExperiment()
 	process = ProcessSimpleExperiment()
+	PSE = PlotSimpleExperiment()
 	process.main()
 
+	# plot_part_nrs = [5]
+	# part_string = ['part_' + str(nr) for nr in plot_part_nrs]
+
+
+
+
 	base_path_raw = "/home/jasper/omni_marco_gazebo/src/stiffness_simple_experiment/figures/part_"
-	AE.generateRawDataPlots(process.all_dfs,base_path_raw)
+	PSE.generateRawDataPlots(process.all_dfs,base_path_raw, [8]) # if want to add specific participant(s)
+	# PSE.generateRawDataPlots(process.all_dfs,base_path_raw) # plot for all participants
+	plt.close()
 
 	base_path = "/home/jasper/omni_marco_gazebo/src/stiffness_simple_experiment/figures/"
-	AE.generateBoxExp(process.real_exp_dfs, process.part_name_list, base_path)
+	# PSE.generateBoxExp(process.real_exp_dfs, process.part_name_list, base_path) # plot for all participants
+	# print(process.part_name_list)
+	# print(process.real_exp_dfs[0].index.values)
+	PSE.generateBoxExp(process.real_exp_dfs, ['part_8'], base_path) # if want to add specific participant(s)
+	plt.close()
 
 	base_path = "/home/jasper/omni_marco_gazebo/src/stiffness_simple_experiment/figures/"
-	AE.generateBoxTypes(process.types_dfs, process.IDstrings, process.part_name_list, base_path)
+	# PSE.generateBoxTypes(process.types_dfs, process.IDstrings, process.part_name_list, base_path) # plot for all participants
+	PSE.generateBoxTypes(process.types_dfs, process.IDstrings, ['part_8'], base_path) # if want to add specific participant(s)
+	plt.close()
 
 	# base_path = "/home/jasper/omni_marco_gazebo/src/stiffness_simple_experiment/figures/"
 	# print(process.real_exp_dfs[0].columns.values)
@@ -217,7 +237,7 @@ if __name__ == "__main__":
 
 
 	# fields = []
-	# AE.generateBoxMetrics(process.real_exp_dfs, fields, process.part_name_list, base_path)
+	# PSE.generateBoxMetrics(process.real_exp_dfs, fields, process.part_name_list, base_path)
 
 	# df2 = pd.DataFrame(np.random.rand(10, 4), columns=['a', 'b', 'c', 'd'])
 	# print(df2)
